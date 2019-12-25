eb847cdd90535182d58f93d47fb5ad900a301b9a
Pradeep <pradeep99909@gmail.com>
2019-12-25 22:13:52 +0530

    process.env.PORT added

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var express = require('express')
 var bodyParser=require('body-parser')
 var app = express()
 var path = require('path')
-var add=require('./function/add')
+var M=require('./function/add')
 const request = require('request');
 const key = require('./function/key/key');
 var geocoder = require('geocoder');
-app.use( bodyParser.json() ); 
+app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')))
 
 // app.get('/',(req,res)=>{
 //     res.sendFile('index.html')
 // })
-
+const Math=new M()
 app.post('/login',(req,res)=>{
-    res.send(add(req.body.a,req.body.b))
+    res.send(Math.add(req.body.a,req.body.b))
 })
 
 app.get('/weather',(req,res)=>{
@@ -32,12 +32,11 @@ app.post('/getweather',(req,res)=>{
             let weather = JSON.parse(body)
             res.send({temp:Math.ceil(weather.main.temp/10),city:weather.name})
         }
-        
     })
 })
 
 app.post('/getcity',(req,res)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     request(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.long}&appid=${key}`,function(err, response, body){
         if(err){
             console.log(err)
@@ -50,4 +49,4 @@ app.post('/getcity',(req,res)=>{
     })
 })
 
-app.listen(8000)
+app.listen(process.env.PORT || 8000)
